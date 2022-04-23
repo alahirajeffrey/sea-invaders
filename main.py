211403353b7d8b3432f664954b8ec8ed8953bb0a
AlahiraJ <alahirajeffrey@gmail.com>
2022-04-23 09:40:35 +0100

    added player icon and coordinates

--- main.py
+++ main.py
@@ -4,13 +4,27 @@ import pygame
 pygame.init()
 
 # create screen
-size = width, height = 800, 600
+size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 
 # set game title and icons
 pygame.display.set_caption("Sea Invaders")
 icon = pygame.image.load("game_image.png")
 pygame.display.set_icon(icon)
+
+# load player image
+player_image = pygame.image.load("spaceship.png")
+
+# set player coordinates
+player_x = 280
+player_y = 350
+
+# add player image and coordinates on screen
+
+
+def player():
+    screen.blit(player_image, (player_x, player_y))
+
 
 # game loop
 running = True
@@ -21,4 +35,8 @@ while running:
 
     # set background colour
     screen.fill((0, 128, 128))
+
+    # call player function after screen.fill
+    player()
+
     pygame.display.update()
