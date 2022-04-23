bdc38cc08edb5b67d961deb62c1f545812de54e5
AlahiraJ <alahirajeffrey@gmail.com>
2022-04-23 21:47:12 +0100

    added enemy movement and background image

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ pygame.init()
 size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 
+# background
+background = pygame.image.load("background.jpg")
+
 # set game title and icons
 pygame.display.set_caption("Sea Invaders")
 icon = pygame.image.load("game_image.png")
@@ -34,8 +37,8 @@ enemy_image = pygame.image.load("enemy.png")
 # set enemy coordinates
 enemy_x = random.randint(0, 280)
 enemy_y = random.randint(50, 100)
-enemy_x_change = 0
-enemy_y_change = 0
+enemy_x_change = 0.5
+enemy_y_change = 20
 
 
 def enemy(x, y):
@@ -53,10 +56,10 @@ while running:
         # check if key is pressed and update player coordinates appropraitely
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                player_x_change += 0.5
+                player_x_change += 1.0
 
             if event.key == pygame.K_LEFT:
-                player_x_change -= 0.5
+                player_x_change -= 1.0
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -65,7 +68,9 @@ while running:
     # set background colour
     screen.fill((0, 128, 128))
 
-    # call player function after screen.fill
+    screen.blit(background, (0, 0))
+
+    # update player x coordinate
     player_x += player_x_change
 
     # set player boundaries
@@ -73,6 +78,17 @@ while running:
         player_x = 0
     elif player_x >= 565:
         player_x = 565
+
+    # update enemy x coordinate
+    enemy_x += enemy_x_change
+
+    # set enemy boundaries
+    if enemy_x <= 0:
+        enemy_x_change = 1.0
+        enemy_y += enemy_y_change
+    elif enemy_x >= 565:
+        enemy_x_change -= 1.0
+        enemy_y += enemy_y_change
 
     # call player function and pass coordinates
     player(player_x, player_y)
