9282432c30fa5f10b792f5f0605e9d2632d552b8
AlahiraJ <alahirajeffrey@gmail.com>
2022-04-23 09:15:12 +0100

    added game window and game loop

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import pygame
 
 # initialize pygame
 pygame.init()
+
+# create screen
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
