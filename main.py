c8888402d33dd296f31dcf09e242a26540bd820d
AlahiraJ <alahirajeffrey@gmail.com>
2022-04-23 12:43:19 +0100

    added enemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # initialize pygame
 pygame.init()
@@ -27,6 +28,21 @@ def player(x, y):
     screen.blit(player_image, (x, y))
 
 
+# load enemy image
+enemy_image = pygame.image.load("enemy.png")
+
+# set enemy coordinates
+enemy_x = random.randint(0, 280)
+enemy_y = random.randint(50, 100)
+enemy_x_change = 0
+enemy_y_change = 0
+
+
+def enemy(x, y):
+    # add enemy image and coordinates on screen
+    screen.blit(enemy_image, (x, y))
+
+
 # game loop
 running = True
 while running:
@@ -37,10 +53,10 @@ while running:
         # check if key is pressed and update player coordinates appropraitely
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                player_x_change += 0.1
+                player_x_change += 0.5
 
             if event.key == pygame.K_LEFT:
-                player_x_change -= 0.1
+                player_x_change -= 0.5
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -60,5 +76,8 @@ while running:
 
     # call player function and pass coordinates
     player(player_x, player_y)
+
+    # call enemy function and pass coordinates
+    enemy(enemy_x, enemy_y)
 
     pygame.display.update()
