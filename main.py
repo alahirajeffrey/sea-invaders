98395394978bdd937c4d27e462801036905db3b0
AlahiraJ <alahirajeffrey@gmail.com>
2022-04-23 09:24:30 +0100

    set backdround colour

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ pygame.init()
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 
+# set game title and icons
+pygame.display.set_caption("Sea Invaders")
+icon = pygame.image.load("game_image.png")
+pygame.display.set_icon(icon)
+
 # game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # set background colour
+    screen.fill((0, 128, 128))
+    pygame.display.update()
